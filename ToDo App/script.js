7850d4387ca52ec52dde33243a0a98af7d609b63
jakubozog <jakub.ozog@sojournsolutions.com>
2021-08-12 13:44:42 +0200

    to do engine updated/additional css to ul added

--- ToDo App/script.js
+++ ToDo App/script.js
@@ -1,18 +1,48 @@
+// Creating variables (input, submit button)
 
-function getInputValue() {            //Input field value checking
+let InputField = document.querySelector(".item-field");
+let SubmitButton = document.querySelector(".submit-new-item");
+let NewListItem = document.querySelector(".new-list-item");
+let ListOfItems = document.querySelector(".items-list");
+
+// Function which corresponds to the adding element on the button click
+
+SubmitButton.addEventListener('click', () => {
+    let InputValue = InputField.value;
+    if(InputValue === ""){
+        alert('napisz coś');
+    } else {
+        let li = document.createElement('li');
+        ListOfItems.innerHTML = InputValue;
+        ListOfItems.insertBefore(li, ListOfItems.childNodes[0]);
+        InputField.value = '';
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* let newItemField = document.querySelector(".item-field");
+
+
+function getInputValue(event) {            //Input field value checking
+    event.preventDefault();
+
+    let NewUserItem = Number(newItemField.value);
     
-    let inputValue = document.querySelector('input');
-    console.log(inputValue);
 }
 
 
-function getSubmit(event) {                       //Submitting value from inputfield to the console log
-    const newItemField = document.querySelector('.item-field').value;
-    getSubmit(value);
-    console.log(getSubmit);
-}
+let submitNewItem = document.querySelector('.submit-new-item');
+submitNewItem.addEventListener('click', getInputValue);
 
-const submitNewItem = document.querySelector('.submit-new-item');
-submitNewItem.addEventListener('click', getSubmit);
-
-// submit-new-item.AddEeventListener("click", CheckInputField );
+// submit-new-item.AddEeventListener("click", CheckInputField ); */
